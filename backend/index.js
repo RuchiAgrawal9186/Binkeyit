b2a14475f9464decef6cb4b2d1116e5d38de9613
Ruchi Agrawal <ruchiagrawal9186@gmail.com>
2025-08-22 15:05:04 +0530

    started backen part connect with DB

---
+++ backend/index.js
@@ -0,0 +1,37 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import morgan from "morgan";
+import helmet from "helmet";
+import connectDB from "./Config/connectDB.js";
+dotenv.config();
+
+const app = express();
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+  })
+);
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(morgan());
+app.use(
+  helmet({
+    crossOriginResourcePolicy: false,
+  })
+);
+
+const PORT = 8080 || process.env.PORT;
+
+app.get("/", (req, res) => {
+  // server to client
+  res.json({ message: "server is running" });
+});
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("server is running in ", PORT);
+  });
+});
